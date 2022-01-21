4baeeb0a3c93b7c10e248ef691504e415c66fdfa
AbhishekMondal1 <Abhishekmondal.official@gmail.com>
2022-01-22 02:38:58 +0530

    add service section cards.

--- client/src/components/Services.jsx
+++ client/src/components/Services.jsx
@@ -4,11 +4,17 @@ import { BiSearchAlt } from 'react-icons/bi'
 import { RiHeart2Fill } from 'react-icons/ri'
 import './services.css';
 
-const ServiceCard = () => {
+const ServiceCard = ({ color, title, icon, subtitle }) => {
     return (
         <div className='servcard white-glassmorphism shadow-xl'>
-
-        </div>
+            <div className={`card-icon ${color}`}>
+                {icon}
+            </div>
+            <div className='card-content'>
+                <h3 className='card-heading'>{title}</h3>
+                <p className='card-para'>{subtitle}</p>
+            </div>
+        </div >
     )
 }
 
@@ -24,6 +30,24 @@ const Services = () => {
                 </div>
             </div>
             <div className='cards'>
+                <ServiceCard
+                    color="bg-[#2952e3]"
+                    title="Security Guranteed"
+                    icon={<BsShieldFillCheck fontSize={21} className='text-white' />}
+                    subtitle="Security is guranteed. We always maintain privacy and maintain the quality of products."
+                />
+                <ServiceCard
+                    color="bg-[#8945f8]"
+                    title="Best exchange rates."
+                    icon={<BiSearchAlt fontSize={21} className='text-white' />}
+                    subtitle="Security is guranteed. We always maintain privacy and maintain the quality of products."
+                />
+                <ServiceCard
+                    color="bg-[#f84550]"
+                    title="Fastest Transactions"
+                    icon={<RiHeart2Fill fontSize={21} className='text-white' />}
+                    subtitle="Security is guranteed. We always maintain privacy and maintain the quality of products."
+                />
             </div>
         </div>
     )
